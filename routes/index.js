470f3b5926fd89a4250045e9df2f1611fc74ae09
Min8828 <minrou9928@gmail.com>
2023-04-20 21:25:10 +0800

    refactor: modify home & urls routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const home = require('./modules/home')
+const urls = require('./modules/urls')
 
 router.use("/", home)
+router.use("/", urls)
 
 module.exports = router
---
+++ routes/modules/urls.js
@@ -0,0 +1,39 @@
+const router = require('express').Router()
+const { customAlphabet } = require('nanoid')
+const ShortenUrl = require('../../models/shortUrl-modal')
+
+router.post('/', async (req, res) => {
+  const { fullUrl } = req.body
+
+  // prevent the empty input from continuing the following steps
+  if (!fullUrl) return res.redirect('/')
+
+  try {
+    const foundUrl = await ShortenUrl.findOne({ fullUrl }).lean().exec()
+    let chars = 'abcdefghijklmnopqrstuvwxzy'
+    chars += chars.toUpperCase() + '0123456789'
+    const shortUrl = customAlphabet(chars, 5)()
+    const url = foundUrl || (await ShortenUrl.create({ fullUrl, shortUrl }))
+    res.render('show', { url })
+  } catch {
+    (err) => console.log(err)
+  }
+})
+
+router.get('/:shortUrl', async (req, res) => {
+  try {
+    const { shortUrl } = req.params
+    const foundUrl = await ShortenUrl.findOne({ shortUrl }).lean().exec()
+
+    if (!foundUrl) {
+      const errorMsg = 'Oops, Cannot found the page'
+      return res.render('error', { errorMsg })
+    }
+
+    return res.redirect(foundUrl.fullUrl)
+  } catch {
+    (err) => console.log(err)
+  }
+})
+
+module.exports = router
