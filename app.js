d17593e70e37ffb4eebc2f79b7445f8f36501b58
Min8828 <minrou9928@gmail.com>
2023-04-20 22:31:42 +0800

    style: modify app.js to standard style

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // use dotenv while in informal env
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
-const express = require("express");
-const engine = require("express-handlebars").engine;
+const express = require('express')
+const engine = require('express-handlebars').engine
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const routes = require("./routes");
-require("./config/mongoose"); // mongoose
+const routes = require('./routes')
+require('./config/mongoose') // mongoose
 
 // setting template engine
 app.engine(
-  "hbs",
+  'hbs',
   engine({
-    layoutsDir: "views/layouts", // directory to handlebars files
-    defaultLayout: "main",
-    extname: "hbs", // specify the file extension as .hbs
+    layoutsDir: 'views/layouts', // directory to handlebars files
+    defaultLayout: 'main',
+    extname: 'hbs', // specify the file extension as .hbs
     runtimeOptions: {
       allowProtoPropertiesByDefault: true,
-      allowProtoMethodsByDefault: true,
-    },
+      allowProtoMethodsByDefault: true
+    }
   })
-);
-app.set("view engine", "hbs");
+)
+app.set('view engine', 'hbs')
 
 // middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // routes
-app.use(routes);
+app.use(routes)
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
